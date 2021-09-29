d671b13c8d4766635b14b75f5d4766ed4f5a2975
LiisaVuorenmaa <liisa.kuisma@hotmail.fi>
2021-09-29 15:00:17 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScreen from './screens/HomeScreen';
+import SearchScreen from './screens/NotificationsScreen';
+import ProfileScreen from './screens/BirdScreen';
 
 export default function App() {
   return (
